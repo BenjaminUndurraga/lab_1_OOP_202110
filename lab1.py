45759f208bd22f24507ed2376a309598eed3510d
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-03-25 23:18:01 -0300

    lab1 progress

---
+++ lab1.py
@@ -0,0 +1,52 @@
+from numpy import random
+import numpy as np
+
+cards_to_play = int(input('with how many pairs of cards you want to play?: '))
+cards = []
+player1=0
+player2=0
+turn = 0
+total_points=0
+
+#crearemos los pares de cartas a jugar
+card_creator = 1
+while card_creator <= cards_to_play:
+    cards.append(card_creator)
+    cards.append(card_creator)
+    card_creator+=1
+'''
+print(cards), estan bien creadas, falta desordenarlas, para esto tendremos 
+que ponerlas en otra lista
+'''
+messy_cards = []
+
+while len(cards)>0:
+    messing_up = cards[random.randint(len(cards))]
+    messy_cards.append(messing_up)
+    cards.remove(messing_up)
+'''
+sacamos las cartas de cards y las pusimos en messy_cards desordenadas.
+ahora tenemos que pasar messy_cards a matriz para que sea un tablero con coordenadas.
+'''
+
+board = np.array(messy_cards).reshape(2,cards_to_play)
+#lo hacemos de dos columnas debido a que 2 es el minimo de cartas totales a jugar
+print(board)
+
+# tenemos el tablero pero falta definir el que lo ocultara
+
+def silenced_board(rows,amount):
+    m=[]
+    for i in range(rows):
+        rows=[]
+        for j in range(amount):
+            rows.append(' *')
+        m.append(rows)
+    return m
+
+move = silenced_board(2,cards_to_play)
+print(move)
+print(board[0][1])
+
+
+
